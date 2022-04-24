f44b254dcbbfe52b251724e23edb99b979189c11
septiintanaml <septiintana@gmail.com>
2022-04-24 14:27:39 +0800

    hhh

--- MidTestSBD/app.py
+++ MidTestSBD/app.py
@@ -3,14 +3,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "HomePage"
 
-@app.route("/login", ['GET','POST'])
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template("login.html")
+5
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template("signup.html")
 
 
 app.run()
